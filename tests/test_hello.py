646e3bd418baaa730a49f6725cd6b435024f7731
akpojotor-shemi <akpojotor.shemi@ibm.com>
2021-11-29 13:46:22 -0500

    update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -2,4 +2,4 @@ def test_hello( client ):
     ret = client.get( '/' )  
     assert ret.status_code == 200
     assert ret.mimetype == 'text/plain'
-    assert b'jet' in ret.data
+    assert b'hello' in ret.data
